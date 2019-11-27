675a81c394384a364e1b060f438633fa2ef1f996
gmonzerol@live.fr <gmonz035@uottawa.ca>
2019-11-27 14:56:42 -0500

    Add pages and entities that have to be completed

--- target/generated-sources/annotations/persistence/Instructor_.java
+++ target/generated-sources/annotations/persistence/Instructor_.java
@@ -3,7 +3,7 @@ package persistence;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-26T19:51:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-27T14:53:14")
 @StaticMetamodel(Instructor.class)
 public class Instructor_ extends User_ {
 
