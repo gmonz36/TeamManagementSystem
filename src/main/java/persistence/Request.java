24f0a8d342e68895c6bc1f1d426e944dd6d731f4
mmede089 <47248680+mmede089@users.noreply.github.com>
2019-11-29 16:07:18 -0500

    Added attributes

--- src/main/java/persistence/Request.java
+++ src/main/java/persistence/Request.java
@@ -5,10 +5,21 @@
  */
 package persistence;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author gmonz36
  */
-public class Request {
+@Entity
+@Table(name="REQUEST_TABLE8930730")
+public class Request implements Serializable{
+    
+    @Id @GeneratedValue long id;
+    boolean status;
     
 }
