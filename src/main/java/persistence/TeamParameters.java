5fe52ad3b6250f357dd0dfd9e52f433f49368eb9
mmede089 <47248680+mmede089@users.noreply.github.com>
2019-11-29 16:08:17 -0500

    Added attributes

--- src/main/java/persistence/TeamParameters.java
+++ src/main/java/persistence/TeamParameters.java
@@ -5,10 +5,59 @@
  */
 package persistence;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author gmonz36
  */
-public class TeamParameters {
+@Entity
+@Table(name="TEAM_PARAMETERS_TABLE8930730")
+public class TeamParameters implements Serializable{
+    
+    
+    @Id
+    String courseCode;    
+    int minStudents;
+    int maxStudents;
+    String deadline;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public int getMinStudents() {
+        return minStudents;
+    }
+
+    public void setMinStudents(int minStudents) {
+        this.minStudents = minStudents;
+    }
+
+    public int getMaxStudents() {
+        return maxStudents;
+    }
+
+    public void setMaxStudents(int maxStudents) {
+        this.maxStudents = maxStudents;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+    
+    
+    
     
 }
