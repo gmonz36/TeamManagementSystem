bd09064c83227d9c01bae1f97071052ccbb52d12
root <gmonz035@uottawa.ca>
2019-11-30 17:51:24 -0500

    fix typo

--- src/main/java/beans/LogInBean.java
+++ src/main/java/beans/LogInBean.java
@@ -81,7 +81,7 @@ public class LogInBean {
                     FacesContext.getCurrentInstance().getExternalContext().redirect("faces/student_protected/create_team.xhtml");
                 } else {
                    session.setAttribute("User", userFacade.findInstructor(userId));
-                   FacesContext.getCurrentInstance().getExternalContext().redirect("faces/instructor_protected/create_team.xhtml");
+                   FacesContext.getCurrentInstance().getExternalContext().redirect("faces/instructor_protected/visualize_teams.xhtml");
                 } 
             } else {
                 status="Invalid Login, Please Try again"; 
