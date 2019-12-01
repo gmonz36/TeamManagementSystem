4943cc2f393b7dce0a50e5acb99850d4a6133e4e
mmede089 <47248680+mmede089@users.noreply.github.com>
2019-11-30 23:10:59 -0500

    Added "Team" Facades

---
+++ src/main/java/beans/TeamFacadeLocal.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package beans;
+
+import persistence.Team;
+import java.util.List;
+import javax.ejb.Local;
+import persistence.Course;
+
+/**
+ *
+ * @author ssome
+ */
+@Local
+public interface TeamFacadeLocal {
+
+    void create(Object user);
+
+    void edit(Object user);
+
+    void remove(Object user);
+
+    Object find(Object id);
+
+    List<Object> findAll();
+
+    List<Object> findRange(int[] range);
+
+    int count();
+    
+    
+    public List<Object> findById(String id);
+    
+    public void addTeamParams(String courseCode,int min_students,int max_students,String deadline);
+    
+    public List<Team> getTeams(String courseCode);
+            
+    public List<Team> getIncompleteTeams(String courseCode);
+    
+    public Course findCourse(String sectionCode);
+    
+    public void createRequest(String status, String userId, String teamId);
+    
+}
