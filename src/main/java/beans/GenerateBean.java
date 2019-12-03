af3c34ca552261dafde4c1e6e42f2f567aa1ef47
genlanzer <glanz030@uottawa.ca>
2019-12-03 11:16:08 -0500

    Added Generator button

---
+++ src/main/java/beans/GenerateBean.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import persistence.Course;
+import persistence.CourseSection;
+
+/**
+ *
+ * @author aman
+ */
+@Named(value = "generateBean")
+@RequestScoped
+public class GenerateBean {
+    @EJB
+    private TeamFacadeLocal teamFacade;
+    @EJB
+    private UserFacadeLocal userFacade;
+    
+    private boolean isGenerated;
+    /**
+     * Creates a new instance of JoinTeamBean
+     */
+    public GenerateBean() {
+    }
+    
+    
+    public void addCourseAndSection(){
+        try{
+            Course course = new Course();
+            course.setCourseCode("SEG3502");
+            CourseSection section = new CourseSection();
+            section.setCourseCode("SEG3502");
+            section.setSectionCode("A01");
+            userFacade.addCourseAndSection(course, section); 
+        }catch(Exception e){
+           System.out.println(e.getMessage());
+        }  
+        
+    }
+    
+    
+    public String isGenerated(){
+        try{
+            
+           if(userFacade.countCourses() == 0){
+               isGenerated = true;
+           }
+           else{
+               isGenerated = false;
+           }
+            
+                
+        
+        }catch(Exception e){
+           System.out.println(e.getMessage());
+        }  
+        return "generate";
+    }
+    
+    
+    
+    public void setIsGenerated(boolean isGenerated){
+        this.isGenerated = isGenerated;
+    }
+    
+    public boolean getIsGenerated(){
+        return isGenerated;
+    }
+    
+    
+    
+    
+   
+    
+
+}
